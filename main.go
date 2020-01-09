e3ec7e0aea28336d575b98abbdc0bd768401f5dd
JaFaR <sysadjafarpr@gmail.com>
2020-01-09 14:30:18 +0300

    fix: fixes #1

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 func main() {
 
 }
-func distance(fuelCons,fuelVol int)  int{
+func distance(fuelConsumption,fuelVolume int)  int{
 	const km  = 100
 	total := 0
-	total = fuelVol*km/fuelCons
+	total = fuelVolume*km/fuelConsumption
 	return total
 }
