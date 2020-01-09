58728b96e7e701c7b3dd58c067d52c73057a9642
JaFaR <sysadjafarpr@gmail.com>
2020-01-09 15:26:00 +0300

    fix: fixes #2

--- main_test.go
+++ main_test.go
@@ -13,9 +13,12 @@ func Test_fuel(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
-		{"One hundred km", 15,15,100},
-		{"Two hundred km", 13,26,200},
-		{"50 km", 10,5,50},
+		{"The volume and fuel consumption are equal", 15,15,100},
+		{"The volume and fuel consumption are equal", 7,7,100},
+		{"Fuel volume is twice as much fuel consumption", 13,26,200},
+		{"Fuel will run out soon", 17,1,5},
+		{"No fuel", 8,0,0},
+		{"No fuel", 3,0,0},
 	}
 	for _, tt := range tests {
 		got := distance(tt.fuelConsumption, tt.fuelVolume)
